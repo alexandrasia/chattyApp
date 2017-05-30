e0602d0c299dbec9dd3d7071cfd34283affc4ae2
Alexandra Sia <alexandrasia@gmail.com>
2017-05-30 16:57:12 -0700

    Extend functionality to handle typing chat message

--- src/ChatBar.jsx
+++ src/ChatBar.jsx
@@ -1,11 +1,51 @@
 import React, {Component} from 'react';
 
 class ChatBar extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      username: this.props.user,
+      message: ''
+    }
+
+  this.onNameChange = this.onNameChange.bind(this);
+  this.onContentChange = this.onContentChange.bind(this);
+  this.onKeyDown = this.onKeyDown.bind(this);
+  }
+
+
+
+  onNameChange(event) {
+    console.log(event.target.value);
+    this.setState({ username: event.target.value });
+  }
+
+  onContentChange(event) {
+    console.log(event.target.value);
+    this.setState({ message: event.target.value });
+  }
+
+  onKeyDown(event) {
+    if (event.key === 'Enter') {
+      console.log('Enter key pressed');
+      this.props.newMessage(this.state.username, this.state.message);
+      console.log(this.state.content);
+    }
+  }
+
   render() {
     return (
       <footer className="chatbar">
-        <input className="chatbar-username" placeholder="Your Name (Optional)" />
-        <input className="chatbar-message" placeholder="Type a message and hit ENTER" />
+        <input className="chatbar-username"
+        placeholder="Your Name (Optional)"
+        value={ this.state.username }
+        onChange={ this.onNameChange }/>
+        <input className="chatbar-message"
+        placeholder="Type a message and hit ENTER"
+        onChange={ this.onContentChange }
+        onKeyDown={ this.onKeyDown }/>
     </footer>
     );
   }
--- src/MessageList.jsx
+++ src/MessageList.jsx
@@ -5,11 +5,27 @@ import Message from './Message.jsx'
 
 class MessageList extends Component {
   render() {
+
+    const messages = this.props.messages.map(message => {
+      return <Message
+        key={ message.id }
+        user={ message.username }
+        content={ message.content } />
+    });
+
     return (
       <main className="messages">
-        <Message/>
+
+        { messages }
+
       </main>
     );
   }
 }
 export default MessageList;
+
+
+
+
+
+
